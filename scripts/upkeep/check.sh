331ce59eb8698a1c69e8114ee691188f786b8614
Opensource <opensource@foursquare.com>
2014-11-10 23:06:32 +0000

    add support for checking a single upkeep task
    also use executable scripts for post-force messages
    
    (sapling split of 7d98604820bd67d9086203b1238de9c6ff0a9f3d)

--- scripts/upkeep/check.sh
+++ scripts/upkeep/check.sh
@@ -9,9 +9,10 @@ set -e
 shopt -s nullglob # without this, empty $DIR would expand * to literal $DIR/*
 
 ran=""
-for req_file in $DIR/required/*; do
-  task=$(basename $req_file)
-  required=$(cat $req_file)
+
+function check() {
+  task=$(basename $1)
+  required=$(cat $1)
 
   current=""
   if [ -f $DIR/current/$task ]; then
@@ -24,7 +25,15 @@ for req_file in $DIR/required/*; do
     run_task $task
     ran="$ran $task"
   fi
-done
+}
+
+if [ "$1" != "" ]; then
+  check $DIR/required/$1
+else
+  for req_file in $DIR/required/*; do
+    check req_file
+  done
+fi
 
 if [ "$ran" != "" ]; then
     echo
