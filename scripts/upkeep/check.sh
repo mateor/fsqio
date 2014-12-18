341f26d814cb034dcebb9c9cdf6f8a5b461a0eca
Opensource <opensource@foursquare.com>
2014-12-18 15:23:52 +0000

    Upkeep Framework Tweaks
     - Add ability to force via ./upkeep
     - Remove ability to force from ./fs
     - Pass in the 'current' value to each task -- tasks like scrub can
    now skip steps intended to remove/recreate state if current is empty
    (e.g. on first run).
    
    (sapling split of 972bcd63c3b3cde80c3ad68cab310b24f33567ed)

--- scripts/upkeep/check.sh
+++ scripts/upkeep/check.sh
@@ -9,13 +9,6 @@ set -e
 shopt -s nullglob # without this, empty $DIR would expand * to literal $DIR/*
 
 ran=""
-
-# Some upkeep tasks are intented for removing bootstrapped state.
-# If they have never been run before, we can assume the state they
-# are intended to remove does not exist either, and just mark them
-# as having run.
-run_on_change_only="clean scrub spindle-upgrade git-shed-resync"
-
 function check() {
   task=$(basename $1)
   required=$(cat $1)
@@ -23,19 +16,11 @@ function check() {
   current=""
   if [ -f $DIR/current/$task ]; then
     current=$(cat $DIR/current/$task)
-  else
-    if [[ "$run_on_change_only" == *"$task"* ]]; then
-      echo "Skipping on-chnage-only $task..."
-      current=$required
-      if [ -f $1 ]; then
-        cp $1 $DIR/current/$task;
-      fi
-    fi
   fi
 
   if [ "$current" != "$required" ]; then
     echo "Running upkeep $task (current: '$current' vs required: '$required')..."
-    run_task $task
+    run_task "$task" "$current"
     ran="$ran $task"
   fi
 }
--- scripts/upkeep/run.sh
+++ scripts/upkeep/run.sh
@@ -4,9 +4,9 @@ DIR=$(dirname ${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]})
 mkdir -p $DIR/current
 
 function run_task() {
-  $DIR/tasks/$(basename $1).sh && if [ -f $DIR/required/$1 ]; then cp $DIR/required/$1 $DIR/current/$1; fi
+  $DIR/tasks/$(basename $1).sh "$2" && if [ -f $DIR/required/$1 ]; then cp $DIR/required/$1 $DIR/current/$1; fi
 }
 
 if [ "$0" = "$BASH_SOURCE" ]; then
-  run_task $1;
+  run_task "$1" "manually";
 fi
