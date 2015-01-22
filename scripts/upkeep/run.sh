c4724bc00cfb7a8cfdde613a47477efd1be9c183
Opensource <opensource@foursquare.com>
2015-01-22 02:28:08 +0000

    replace bootstrap/dependencies.sh with upkeep tasks
    breaks the monolithic bootstrap into several upkeep tasks for downloading and installing each thing.
    
    Wash is pretty stupid now
    
    (sapling split of ec80938f79fe0f06c42796de6bfdb8d9fc242593)

--- scripts/upkeep/run.sh
+++ scripts/upkeep/run.sh
@@ -4,9 +4,30 @@ DIR=$(dirname ${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]})
 mkdir -p $DIR/current
 
 function run_task() {
+  export BUILD_ROOT=$(dirname $(dirname $DIR))
+  export BOOTSTRAP_DIR=${BUILD_ROOT}/.pants.bootstrap
+  export FS_DEV_BUCKET=http://bodega.prod.foursquare.com/4sq-dev
+  export URL_BASE=$FS_DEV_BUCKET/data/bin
+  export CURL_PROXY="proxyout-aux:80"
+
   $DIR/tasks/$(basename $1).sh "$2" && if [ -f $DIR/required/$1 ]; then cp $DIR/required/$1 $DIR/current/$1; fi
 }
 
-if [ "$0" = "$BASH_SOURCE" ]; then
-  run_task "$1" "manually";
-fi
+function do_in_sudo {
+  if [ -z "$NO_SUDO_BOOTSTRAP" ]; then
+    sudo "$@"
+  else
+    echo "NO_SUDO_BOOTSTRAP enabled. Will not perform: " "$@" 1>&2
+    exit 1
+  fi
+}
+
+for task in "$@"; do
+  if [ "$0" = "$BASH_SOURCE" ]; then
+    if [ $# -gt 1 ]; then
+      echo "Running $task upkeep..."
+    fi
+    run_task "$task" "manually";
+  fi
+done
+
