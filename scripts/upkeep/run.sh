d2286ad48ffaf7682d0212a77bf7b2089c5d4758
Opensource <opensource@foursquare.com>
2015-02-03 22:14:37 +0000

    upkeep install jdk8
    This still requires JAVA_HOME point to JDK7 for now, but should get JDK8 out and available for testing.
    
    (sapling split of 31a3280f93130b49a6b968de4c7d2751d05c747a)

--- scripts/upkeep/run.sh
+++ scripts/upkeep/run.sh
@@ -14,15 +14,6 @@ function run_task() {
   $DIR/tasks/$(basename $1).sh "$2" && if [ -f $DIR/required/$1 ]; then cp $DIR/required/$1 $DIR/current/$1; fi
 }
 
-function do_in_sudo {
-  if [ -z "$NO_SUDO_BOOTSTRAP" ]; then
-    sudo "$@"
-  else
-    echo "NO_SUDO_BOOTSTRAP enabled. Will not perform: " "$@" 1>&2
-    exit 1
-  fi
-}
-
 for task in "$@"; do
   if [ "$0" = "$BASH_SOURCE" ]; then
     if [ $# -gt 1 ]; then
