bad89a1843cae2efbeb1cee66748c817735c2ed0
Opensource <opensource@foursquare.com>
2015-09-15 19:49:04 +0000

    hack to get around hive not knowing how to deprecate things
    (sapling split of b8cf4babffe3f8d2bdb55aa4f86ee875a61b857e)

---
+++ src/jvm/io/fsq/hack/hive/GenericUDAFEvaluatorAggregationBuffer.scala
@@ -0,0 +1,6 @@
+// Copyright 2015 Foursquare Labs, Inc. All Rights Reserved.
+
+package io.fsq.hack.hive
+
+trait GenericUDAFEvaluatorAggregationBuffer extends org.apache.hadoop.hive.ql.udf.generic.GenericUDAFEvaluator.AggregationBuffer {
+}
