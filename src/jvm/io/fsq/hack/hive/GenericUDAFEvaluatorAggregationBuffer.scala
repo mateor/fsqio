3bf46f266c8bd0f9f1a5935ead02234e54ad2457
Opensource <opensource@foursquare.com>
2015-09-15 20:18:47 +0000

    use type instead of trait for hive hack
    (sapling split of 1da239b52d7d819ceca4a173ab5c6249c5986e98)

--- src/jvm/io/fsq/hack/hive/GenericUDAFEvaluatorAggregationBuffer.scala
+++
@@ -1,6 +0,0 @@
-// Copyright 2015 Foursquare Labs, Inc. All Rights Reserved.
-
-package io.fsq.hack.hive
-
-trait GenericUDAFEvaluatorAggregationBuffer extends org.apache.hadoop.hive.ql.udf.generic.GenericUDAFEvaluator.AggregationBuffer {
-}
