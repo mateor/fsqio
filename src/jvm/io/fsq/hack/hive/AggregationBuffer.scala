ce11524860bfa6f0ea60df1910fcd05eca35e80e
Opensource <opensource@foursquare.com>
2015-09-15 20:49:15 +0000

    delete hive hack
    (sapling split of 24ac9674bb9d61e7f5fbdb43dfae296a7ce4d79e)

--- src/jvm/io/fsq/hack/hive/AggregationBuffer.scala
+++
@@ -1,7 +0,0 @@
-// Copyright 2015 Foursquare Labs, Inc. All Rights Reserved.
-
-package io.fsq.hack.hive
-
-object GenericUDAFEvaluator {
-  type AggregationBuffer = org.apache.hadoop.hive.ql.udf.generic.GenericUDAFEvaluator.AggregationBuffer
-}
