6109582e59526661b400ee13ba107e613c869023
Opensource <opensource@foursquare.com>
2015-11-04 16:06:13 +0000

    Revert "Refactor upkeep pipeline to support multiple pants.ini files."
    This reverts commit b1f7bd50d0c4fc8f745f8e7d969ea080a40ba17e.
    
    (sapling split of 9646b39d47dd086ba3cecab7c5c18d52fddd39f2)

--- scripts/upkeep/run.sh
+++ scripts/upkeep/run.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-# Copyright 2015 Foursquare Labs Inc. All Rights Reserved.
-
-# Pass variables to children
-set -a
 
 DIR=$(dirname ${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]})
-mkdir -p "$DIR"/current
+mkdir -p $DIR/current
 
 function run_task() {
   export BUILD_ROOT=$(dirname $(dirname $DIR))
   export BOOTSTRAP_DIR=${BUILD_ROOT}/.pants.bootstrap
   export FS_DEV_BUCKET=http://bodega.prod.foursquare.com/4sq-dev
-  export URL_BASE="$FS_DEV_BUCKET"/data/bin
+  export URL_BASE=$FS_DEV_BUCKET/data/bin
   export CURL_PROXY="proxyout-aux:80"
   export PYTHONPATH=""
-  task_script="$DIR"/"$TASKS"/$(basename $1).sh
-  "$task_script" "$2" && if [ -f "$DIR"/required/$1 ]; then cp "$DIR"/required/$1 "$DIR"/current/$1; fi
+
+  $DIR/tasks/$(basename $1).sh "$2" && if [ -f $DIR/required/$1 ]; then cp $DIR/required/$1 $DIR/current/$1; fi
 }
 
 for task in "$@"; do
