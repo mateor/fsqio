dcb770254d34acd37ec41a51aed5152a76412d64
Opensource <opensource@foursquare.com>
2015-11-06 03:12:05 +0000

    Revert "Update tasks to use new upkeep and re-add JDK handling to pants script"
    This reverts commit 8c8c4f025997685091ff849c0059e95f307e7ded.
    
    (sapling split of 72ae07eced1847555cbe21e8f7213c68e8f79261)

--- scripts/upkeep/run.sh
+++ scripts/upkeep/run.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-# Copyright 2015 Foursquare Labs Inc. All Rights Reserved.
-
-# Pass variables to children
-set -a
-
-
-# Please do NOT run this script directly, interact with the upkeep script instead.
 
 DIR=$(dirname ${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]})
-
-mkdir -p "$DIR"/current
-
-source "$DIR"/"$TASKS"/env.sh
+mkdir -p $DIR/current
 
 function run_task() {
-  task_script="$DIR"/"$TASKS"/$(basename $1).sh
-  "$task_script" "$2" && if [ -f "$DIR"/required/$1 ]; then cp "$DIR"/required/$1 "$DIR"/current/$1; fi
+  export BUILD_ROOT=$(dirname $(dirname $DIR))
+  export BOOTSTRAP_DIR=${BUILD_ROOT}/.pants.bootstrap
+  export FS_DEV_BUCKET=http://bodega.prod.foursquare.com/4sq-dev
+  export URL_BASE=$FS_DEV_BUCKET/data/bin
+  export CURL_PROXY="proxyout-aux:80"
+  export PYTHONPATH=""
+
+  $DIR/tasks/$(basename $1).sh "$2" && if [ -f $DIR/required/$1 ]; then cp $DIR/required/$1 $DIR/current/$1; fi
 }
 
 for task in "$@"; do
