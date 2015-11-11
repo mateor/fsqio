55c6315a62fa0e34d3b9951887f9a394183624d5
Opensource <opensource@foursquare.com>
2015-11-11 14:56:39 -0500

    geocoder hotfix: use public example
    (sapling split of cdfd775e024092e6afea4b1997b0d56da026c2ff)

--- src/jvm/io/fsq/twofishes/server/JsonHotfixFileBuilder.scala
+++ src/jvm/io/fsq/twofishes/server/JsonHotfixFileBuilder.scala
@@ -10,10 +10,10 @@ import org.apache.thrift.TSerializer
 object JsonHotfixFileBuilder {
   // build individual objects here and run build-hotfix-file.py to produce json file
   // DO NOT push commits with changes to this list back to twofishes
-/* example:
+/* example: adding UES name to Upper East Side
     GeocodeServingFeatureEdit.newBuilder
       .editType(EditType.Modify)
-      .longId(7661L)
+      .longId(72057594044543258L)
       .namesEdits(List(FeatureNameListEdit.newBuilder
         .editType(EditType.Add)
         .name("UES")
