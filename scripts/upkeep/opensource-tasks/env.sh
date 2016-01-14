4fdb00f9da2806f2c002cbb8b4059c26c06905f1
Mateo Rodriguez <mateo@foursquare.com>
2016-01-14 17:18:30 +0000

    Use an Fsqio specific pants venv.
    This is going to be still using the homedir. We considered
    moving under the buildroot but since the install is done
    atomically I am going to leave it as is for now.
    
    (sapling split of dc846ee733cccf02b4e43efb85a13a658ff02142)

--- scripts/upkeep/opensource-tasks/env.sh
+++ scripts/upkeep/opensource-tasks/env.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Copyright 2015 Foursquare Labs Inc. All Rights Reserved.
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PANTS_CONFIG_OVERRIDE="['pants.ini']"
-export FSQIO_HOME="$DIR"
+
 # The script below runs the pants bootstrap task and exports PANTSBINARY. Basically a noop if the pants_version
 # hasn't changed. This could be hooked more properly into upkeep but we are waiting on the need to arise.
 source scripts/upkeep/opensource-tasks/opensource-pants-env.sh
